a402aa6ea2f9c40b0e5ef426614e33fa12e95591
Ed Baskerville <ed@edbaskerville.com>
2017-08-09 12:57:20 -0700

    Header generation working.

--- generate_manager.py
+++ generate_manager.py
@@ -42,6 +42,8 @@ def main():
     print(typeinfo.name)
     print(typeinfo.columns)
     print(typeinfo.reflists)
+    
+    generate_manager_header(output_dir, type_header_prefix, typeinfo)
 
 def parse_arguments():
     '''Parses command-line arguments.'''
@@ -99,6 +101,82 @@ def parse_type(input_file):
                 reflists.append((tokens[1], tokens[2][:-1]))
     
     return TypeInfo(name, columns, reflists)
+
+manager_header_format = '''#ifndef {manager_type_header_prefix}_hpp
+#define {manager_type_header_prefix}_hpp
+
+#include "{object_type_header_prefix}.hpp"
+#include "IndexedMap.hpp"
+
+struct {manager_type} {{
+    uint64_t next_id;
+    IndexedMap<{object_type}> populations;
+    
+    // Constructors
+    {manager_type}();
+    {manager_type}(uint64_t next_id);
+    
+    // Object management
+    {object_type} * create();
+    {object_type} * create(uint64_t id);
+    {object_type} * object_for_id(uint64_t id);
+    
+    // Checkpointing methods
+    void load_from_checkpoint(sqlite3 * db, char const * const table_name);
+    {resolve_relationships_signature}
+    {save_to_checkpoint_signature}
+}}
+
+#endif // #define {manager_type_header_prefix}_hpp
+'''
+
+resolve_relationships_signature_format = \
+    'void resolve_relationships(sqlite3 * db, char const * const table_name{ref_table_name_args});'
+
+save_to_checkpoint_signature_format = \
+    'void save_to_checkpoint(sqlite3 * db, char const * const table_name{reflist_table_name_args});'
+
+def generate_manager_header(output_dir, object_type_header_prefix, typeinfo):
+    manager_type_header_prefix = object_type_header_prefix + 'Manager'
+    manager_header_filename = os.path.join(output_dir, manager_type_header_prefix + '.hpp')
+    print(manager_header_filename)
+    if os.path.exists(manager_header_filename):
+        print('{} already exists.'.format(manager_header_filename))
+        sys.exit(1)
+    
+    object_type = typeinfo.name
+    manager_type = object_type + 'Manager'
+    
+    def format_manager_header():
+        ref_vars = [c[2] for c in typeinfo.columns]
+        reflist_vars = [rl[1] for rl in typeinfo.reflists]
+        
+        def format_table_name_args(vars):
+            if len(vars) == 0:
+                return ''
+            return ', ' + ', '.join(['char const * const {}_table_name'.format(var) for var in vars])
+        
+        def format_resolve_relationships_signature():
+            return resolve_relationships_signature_format.format(
+                ref_table_name_args = format_table_name_args(ref_vars + reflist_vars)
+            )
+        
+        def format_save_to_checkpoint_signature():
+            return save_to_checkpoint_signature_format.format(
+                reflist_table_name_args = format_table_name_args(reflist_vars)
+            )
+        
+        return manager_header_format.format(
+            object_type_header_prefix = object_type_header_prefix,
+            object_type = object_type,
+            manager_type_header_prefix = manager_type_header_prefix,
+            manager_type = manager_type,
+            resolve_relationships_signature = format_resolve_relationships_signature(),
+            save_to_checkpoint_signature = format_save_to_checkpoint_signature()
+        )
+    
+    with open(manager_header_filename, 'w') as output_file:
+        output_file.write(format_manager_header())
 
 if __name__ == '__main__':
     main()
